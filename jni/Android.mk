76ba8b5e78b363d62d95194749fc822b53afb53c
Juha Heinanen <jh@tutpro.com>
2021-10-07 16:20:06 +0300

    Make only AEC mobile mode (aecm)

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,23 +1,16 @@
 
 LOCAL_PATH := $(call my-dir)
 
-common_CFLAGS := -fexceptions -DWEBRTC_POSIX=1 -DWEBRTC_ANDROID -I$(LOCAL_PATH)/src/
+common_CFLAGS := -fexceptions -DWEBRTC_POSIX=1 -DWEBRTC_ANDROID -I$(LOCAL_PATH)/src/webrtc/
 common_LDFLAGS :=
 
 common_SRC_FILES := \
-	src/webrtc/modules/audio_processing/aec/aec_core.c                    \
-	src/webrtc/modules/audio_processing/aec/aec_rdft.c                    \
-	src/webrtc/modules/audio_processing/aec/aec_resampler.c               \
-	src/webrtc/modules/audio_processing/aec/echo_cancellation.c           \
-	src/webrtc/modules/audio_processing/aecm/aecm_core.c                  \
-	src/webrtc/modules/audio_processing/aecm/echo_control_mobile.c        \
-	src/webrtc/modules/audio_processing/ns/noise_suppression.c            \
-	src/webrtc/modules/audio_processing/ns/noise_suppression_x.c          \
-	src/webrtc/modules/audio_processing/ns/ns_core.c                      \
-	src/webrtc/modules/audio_processing/ns/nsx_core.c                     \
-	src/webrtc/modules/audio_processing/utility/delay_estimator_wrapper.c \
-	src/webrtc/modules/audio_processing/utility/delay_estimator.c         \
-	src/webrtc/common_audio/fft4g.c                                       \
+	src/webrtc/modules/audio_processing/aecm/aecm_core.cc                  \
+	src/webrtc/modules/audio_processing/aecm/echo_control_mobile.cc        \
+	src/webrtc/modules/audio_processing/utility/delay_estimator_wrapper.cc \
+	src/webrtc/modules/audio_processing/utility/delay_estimator.cc         \
+	src/webrtc/common_audio/third_party/ooura/fft_size_256/fft4g.cc       \
+	src/webrtc/common_audio/third_party/spl_sqrt_floor/spl_sqrt_floor.c   \
 	src/webrtc/common_audio/ring_buffer.c                                 \
 	src/webrtc/common_audio/signal_processing/complex_bit_reverse.c       \
 	src/webrtc/common_audio/signal_processing/complex_fft.c               \
@@ -32,18 +25,13 @@ common_SRC_FILES := \
 	src/webrtc/common_audio/signal_processing/real_fft.c                  \
 	src/webrtc/common_audio/signal_processing/spl_init.c                  \
 	src/webrtc/common_audio/signal_processing/spl_sqrt.c                  \
-	src/webrtc/common_audio/signal_processing/spl_sqrt_floor.c            \
 	src/webrtc/common_audio/signal_processing/vector_scaling_operations.c
 
 
 ifneq ($(findstring arm,$(TARGET_ARCH_ABI)),)
 	common_SRC_FILES += \
-	src/webrtc/modules/audio_processing/aec/aec_core_neon.c               \
-	src/webrtc/modules/audio_processing/aec/aec_rdft_neon.c               \
-	src/webrtc/modules/audio_processing/aecm/aecm_core_c.c                \
-	src/webrtc/modules/audio_processing/aecm/aecm_core_neon.c             \
-	src/webrtc/modules/audio_processing/ns/nsx_core_c.c                   \
-	src/webrtc/modules/audio_processing/ns/nsx_core_neon.c                \
+	src/webrtc/modules/audio_processing/aecm/aecm_core_neon.cc            \
+	src/webrtc/modules/audio_processing/aecm/aecm_core_c.cc               \
 	src/webrtc/common_audio/signal_processing/cross_correlation_neon.c    \
 	src/webrtc/common_audio/signal_processing/downsample_fast_neon.c      \
 	src/webrtc/common_audio/signal_processing/min_max_operations_neon.c
@@ -58,26 +46,6 @@ ifneq ($(findstring arm,$(TARGET_ARCH_ABI)),)
 		common_CFLAGS += -DWEBRTC_ARCH_ARM64
 	endif
 
-else ifeq ($(TARGET_ARCH_ABI), mips)
-	common_SRC_FILES += \
-	src/webrtc/modules/audio_processing/aec/aec_core_mips.c               \
-	src/webrtc/modules/audio_processing/aec/aec_rdft_mips.c               \
-	src/webrtc/modules/audio_processing/aecm/aecm_core_mips.c             \
-	src/webrtc/modules/audio_processing/ns/nsx_core_mips.c                \
-	src/webrtc/common_audio/signal_processing/cross_correlation_mips.c    \
-	src/webrtc/common_audio/signal_processing/downsample_fast_mips.c      \
-	src/webrtc/common_audio/signal_processing/min_max_operations_mips.c
-
-	common_CFLAGS += -DMIPS_FPU_LE
-
-else ifneq ($(findstring x86,$(TARGET_ARCH_ABI)),)
-	common_SRC_FILES += \
-	src/webrtc/modules/audio_processing/aec/aec_core_sse2.c               \
-	src/webrtc/modules/audio_processing/aec/aec_rdft_sse2.c               \
-	src/webrtc/modules/audio_processing/aecm/aecm_core_c.c                \
-	src/webrtc/modules/audio_processing/ns/nsx_core_c.c                   \
-	src/webrtc/system_wrappers/source/cpu_features.cc
-
 endif
 
 common_C_INCLUDES = $(LOCAL_PATH)/include
@@ -87,7 +55,7 @@ LOCAL_MODULE:= libwebrtc
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_LDFLAGS += $(common_LDFLAGS)
+#LOCAL_LDFLAGS += $(common_LDFLAGS)
 LOCAL_C_INCLUDES += $(common_C_INCLUDES)
 
 #include $(BUILD_SHARED_LIBRARY)
